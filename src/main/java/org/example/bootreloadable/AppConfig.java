667fb7599997343514782087b6bf8c3650db184a
Bayo Erinle <berinle@gmail.com>
2024-04-11 09:18:40 -0400

    using properties instead

--- src/main/java/org/example/bootreloadable/AppConfig.java
+++ src/main/java/org/example/bootreloadable/AppConfig.java
@@ -1,9 +1,19 @@
 package org.example.bootreloadable;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Slf4j
 @Configuration
 public class AppConfig {
 
@@ -14,8 +24,34 @@ public class AppConfig {
     @Value("${CREDENTIALS_SECRET}")
     String secret;
 
+    @Getter
+    private Map<String, String> config;
+    private String configFilePath;
+
+    public AppConfig(@Value("${config.file.path}") String configFilePath) {
+        this.configFilePath = configFilePath;
+        reloadConfig();
+    }
+
+    public void reloadConfig() {
+        Properties properties = new Properties();
+        try {
+            properties.load(Files.newInputStream(Paths.get(configFilePath)));
+        } catch (IOException e) {
+            log.error("Error loading configuration:", e);
+        }
+        config = new HashMap<>();
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            config.put(String.valueOf(entry.getKey()), (String) entry.getValue());
+        }
+
+        System.out.println("config = " + config);
+
+        System.out.println("** env key=" + System.getenv("CREDENTIALS_KEY"));
+        System.out.println("** env secret=" + System.getenv("CREDENTIALS_SECRET"));
+    }
+
     @Bean
-//    @RefreshScope
     public AppBean appBean() {
         return new AppBean(key, secret);
     }
--- src/main/java/org/example/bootreloadable/AppController.java
+++ src/main/java/org/example/bootreloadable/AppController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 // https://www.baeldung.com/spring-reinitialize-singleton-bean
 
@@ -24,13 +27,43 @@ public class AppController {
     String secret;
     private Environment environment;
 
+    private AppConfig appConfig;
+
     @Autowired
-    public AppController(AppBean appBean, ApplicationContext ctx, Environment environment) {
+    public AppController(AppBean appBean, ApplicationContext ctx, Environment environment, AppConfig appConfig) {
         this.appBean = appBean;
         this.ctx = ctx;
         this.environment = environment;
+        this.appConfig = appConfig;
     }
 
+
+    @GetMapping("/magic")
+    public void magic() {
+        appConfig.reloadConfig();
+
+        Map<String, String> config = appConfig.getConfig();
+
+
+        DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) ctx.getAutowireCapableBeanFactory();
+        registry.destroySingleton("appBean");
+        registry.registerSingleton("appBean", new AppBean(config.get("k1"), config.get("k2")));
+
+        //re-fetch bean from context
+        this.appBean = (AppBean) registry.getSingleton("appBean");
+
+        System.out.println("appBean = " + appBean);
+    }
+    @GetMapping("/reinitialize")
+    public void reinitialize(@RequestParam("key") String _key, @RequestParam("secret") String _secret) {
+
+        DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) ctx.getAutowireCapableBeanFactory();
+        registry.destroySingleton("appBean");
+        registry.registerSingleton("appBean", new AppBean(_key, _secret));
+
+        //re-fetch bean from context
+        this.appBean = (AppBean) registry.getSingleton("appBean");
+    }
 
     @GetMapping("/ping")
     public String hello() {
@@ -40,10 +73,6 @@ public class AppController {
 
         System.out.println("** env key=" + System.getenv("CREDENTIALS_KEY"));
         System.out.println("** env secret=" + System.getenv("CREDENTIALS_SECRET"));
-
-        DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) ctx.getAutowireCapableBeanFactory();
-        registry.destroySingleton("appBean");
-        registry.registerSingleton("appBean", new AppBean(key, secret));
 
         System.out.println("appBean = " + appBean);
         return appBean.getKey() + "->" + appBean.getSecret();
